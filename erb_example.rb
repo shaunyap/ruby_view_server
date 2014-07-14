cf5a6782d9e98a8b9361ea569157f27afe6cc325
Shaun Yap <shaunyap@Shauns-MacBook-Air.local>
2014-07-14 22:11:45 +0400

    erb homework

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,11 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+a = 33
+template2 = ERB.new "The value of a is: <%= a %>"
+puts template2.result(binding)
+
+y = ["Ferrari", "Mercedez", "Volkswagen"]
+fast = ERB.new "The fastest car is: <%= y[0] %>"
+puts fast.result(binding)
